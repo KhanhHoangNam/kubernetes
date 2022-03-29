9f50b378f09cf643177dabe28263558b81676a0b
khanhhn <chienbinhbattu9xpro@gmail.com>
2022-03-29 10:49:37 +0700

    remove redundant files

--- mariadb/install.sh
+++ mariadb/install.sh
@@ -5,3 +5,28 @@ helm install my-mariadb bitnami/mariadb --version 10.4.2
 helm pull 
 
 https://charts.bitnami.com/bitnami/bitnami/mariadb --version 10.4.2
+
+#1. 
+kubectl create -f mariadb-pv-hostpath.yaml 
+
+#2.
+kubectl create -f  mariadbslave-pv-hostpath.yaml 
+
+#3. 
+kubectl create -f  mariadb-pvc.yaml 
+
+#4.
+kubectl create -f  mariadbslave-pvc.yaml 
+
+#5.
+helm install --name mariadb --set master.persistence.existingClaim=mariadb-pvc stable/mariadb #Helm 2
+helm install mariadb --set master.persistence.existingClaim=mariadb-pvc stable/mariadb #Helm 3
+
+#6. Password: gFyTBqHjTz
+
+echo Password: $(kubectl get secret --namespace default mariadb -o jsonpath="{.data.mariadb-root-password}" | base64 --decode)
+password: c0sgBCE9Sx; puJzm2E7m2; W2VJovbUI5
+mysql -u root -p$MARIADB_ROOT_PASSWORD
+mysql -h mariadb.default.svc.cluster.local -uroot -pW2VJovbUI5
+mysql -h 10.106.115.209:3306 -uroot -p my_database
+mysql -h 192.168.56.11 -u root -p$MARIADB_ROOT_PASSWORD
