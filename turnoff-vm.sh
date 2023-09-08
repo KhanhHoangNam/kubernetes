4c9872dd09c3198f888b775aa8d63058147f435e
khanhhn <chienbinhbattu9xpro@gmail.com>
2023-09-08 22:21:22 +0700

    Update code

---
+++ turnoff-vm.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd haproxy-01 && vagrant halt && \
+cd ../haproxy-02 && vagrant halt && \
+cd ../master-01 && vagrant halt && \
+cd ../master-02 && vagrant halt && \
+cd ../master-03 && vagrant halt && \
+cd ../worker-01 && vagrant halt && \
+cd ../worker-02 && vagrant halt && \
+cd ../worker-03 && vagrant halt && \
+cd ../devci && vagrant halt
--- turnon-vm.sh
+++ turnon-vm.sh
@@ -6,4 +6,5 @@ cd ../master-02 && vagrant reload && \
 cd ../master-03 && vagrant reload && \
 cd ../worker-01 && vagrant reload && \
 cd ../worker-02 && vagrant reload && \
-cd ../worker-03 && vagrant reload
+cd ../worker-03 && vagrant reload && \
+cd ../devci && vagrant reload
