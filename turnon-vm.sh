f303ac0df6399a645fe80fd7bbb77b44f6c8b56e
khanhhn <chienbinhbattu9xpro@gmail.com>
2023-11-16 20:48:53 +0700

    [Develop] Update code

--- turnon-vm.sh
+++ turnon-vm.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-cd haproxy-01 && vagrant reload && \
-cd ../haproxy-02 && vagrant reload && \
-cd ../master-01 && vagrant reload && \
+cd master-01 && vagrant reload && \
 cd ../master-02 && vagrant reload && \
 cd ../master-03 && vagrant reload && \
 cd ../worker-01 && vagrant reload && \
@@ -9,3 +7,5 @@ cd ../worker-02 && vagrant reload && \
 cd ../worker-03 && vagrant reload && \
 cd ../rancher && vagrant reload && \
 cd ../devci && vagrant reload
+cd ../haproxy-01 && vagrant reload && \
+cd ../haproxy-02 && vagrant reload && \
