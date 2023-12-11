09969abb160d3776af2bd9e4b8eb8c81aef9a54d
khanhhn <chienbinhbattu9xpro@gmail.com>
2023-12-11 21:29:37 +0700

    [Develop] Update code

--- turnoff-vm.sh
+++ turnoff-vm.sh
@@ -7,5 +7,7 @@ cd ../master-03 && vagrant halt && \
 cd ../worker-01 && vagrant halt && \
 cd ../worker-02 && vagrant halt && \
 cd ../worker-03 && vagrant halt && \
+cd ../worker-04 && vagrant halt && \
+cd ../worker-05 && vagrant halt && \
 cd ../rancher && vagrant halt && \
 cd ../devci && vagrant halt
--- turnon-vm.sh
+++ turnon-vm.sh
@@ -5,6 +5,8 @@ cd ../master-03 && vagrant reload && \
 cd ../worker-01 && vagrant reload && \
 cd ../worker-02 && vagrant reload && \
 cd ../worker-03 && vagrant reload && \
+cd ../worker-04 && vagrant reload && \
+cd ../worker-05 && vagrant reload && \
 cd ../rancher && vagrant reload && \
 cd ../devci && vagrant reload && \
 cd ../haproxy-01 && vagrant reload && \
